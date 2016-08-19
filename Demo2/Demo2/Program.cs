596920f93bac0e70c2053ea8a06186395fbd9323
AndFueHer <crazyz.224@gmail.com>
2016-08-18 18:44:50 -0600

    Menu mas metodo CtoF

--- Demo2/Demo2/Program.cs
+++ Demo2/Demo2/Program.cs
@@ -10,6 +10,57 @@ namespace Demo2
     {
         static void Main(string[] args)
         {
+            Console.Title = "Conversión de temperatura";
+
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Seleccione una opción: ");
+
+                Console.WriteLine("1. Celsius a Fahrenheit");
+                Console.WriteLine("2. Fahrenheit a Celsius");
+
+                string opcion = Console.ReadLine();
+
+                if (opcion == "1")
+                {
+                    CtoF();
+                }
+
+                if (opcion == "2")
+                {
+                    //FtoC();
+                }
+
+                Console.WriteLine("\nSeleccione una opción: ");
+                Console.WriteLine("1. Volver al menú principal");
+                Console.WriteLine("2. Salir");
+
+                opcion = Console.ReadLine();
+
+                if (opcion != "1")
+                {
+                    continuar = false;
+                }
+            }
+        }
+        static void CtoF() 
+        {
+            Console.Clear();
+
+            Console.WriteLine("Celsius a Fahrenheit");
+            Console.WriteLine("");
+
+            Console.WriteLine("Ingrese el valor de Celsius a convertir: ");
+            double cels = double.Parse(Console.ReadLine());
+            
+
+            double resFahr = (cels*(9/5))+32;
+
+            Console.WriteLine("El resultado es: " + resFahr);
         }
     }
 }
